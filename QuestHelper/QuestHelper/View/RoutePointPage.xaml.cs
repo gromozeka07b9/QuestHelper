72bf50beb19eeb60f38bf4a6e1fceaf276888b53
gromozeka07b9 <gromozeka07b9@gmail.com>
2018-06-28 08:32:30 +0300

    вывод координат

--- QuestHelper/QuestHelper/View/RoutePointPage.xaml.cs
+++ QuestHelper/QuestHelper/View/RoutePointPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuestHelper.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,8 @@ namespace QuestHelper.View
 		public RoutePointPage ()
 		{
 			InitializeComponent ();
-		}
+            BindingContext = new RoutePointViewModel() { Navigation = this.Navigation };
+        }
 
     }
 }
--- QuestHelper/QuestHelper/ViewModel/RoutePointViewModel.cs
+++ QuestHelper/QuestHelper/ViewModel/RoutePointViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using QuestHelper.Model.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,12 +20,34 @@ namespace QuestHelper.ViewModel
         public ICommand UpdateCommand { get; private set; }
         public ICommand DeleteCommand { get; private set; }
 
+        RoutePoint _point = new RoutePoint();
+        string _currentPositionString = string.Empty;
+
         public RoutePointViewModel()
         {
             CreateCommand = new Command(createRoutePoint);
             UpdateCommand = new Command(updateRoutePoint);
             DeleteCommand = new Command(deleteRoutePoint);
+            fillCurrentPositionAsync();
+            //currentPosition.
         }
+
+        private async void fillCurrentPositionAsync()
+        {
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+            _point.Latitude = position.Latitude;
+            _point.Longitude = position.Longitude;
+            Coordinates = _point.Latitude + "," + _point.Longitude;
+            //Coordinates = _currentPositionString;
+        }
+
+        /*private async Task<Position> GetCurrentPositionAsync()
+        {
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+            return position;
+        }*/
 
         async void createRoutePoint()
         {
@@ -33,6 +59,52 @@ namespace QuestHelper.ViewModel
         async void deleteRoutePoint()
         {
 
+        }
+
+        public string Coordinates
+        {
+            set
+            {
+                if (_currentPositionString != value)
+                {
+                    _currentPositionString = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Coordinates"));
+                }
+            }
+            get
+            {
+                return _currentPositionString;
+            }
+        }
+        public string Name
+        {
+            set
+            {
+                if (_point.Name != value)
+                {
+                    _point.Name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+            get
+            {
+                return _point.Name;
+            }
+        }
+        public string Description
+        {
+            set
+            {
+                if (_point.Description != value)
+                {
+                    _point.Description = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+                }
+            }
+            get
+            {
+                return _point.Description;
+            }
         }
     }
 }
