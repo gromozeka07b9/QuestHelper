c0c270742b64f02ec6296235afb6db7495ba93f8
sergey.dyachenko <sergey.dyachenko@aeroclub.ru>
2020-02-08 12:56:56 +0300

    признак отладки

---
+++ QuestHelper/QuestHelper/Consts/TestMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestHelper.Consts
+{
+    public class TestMode
+    {
+        public static bool IsTestMode
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}
