2d5acf4fe999921b25df1f3e1354fdf9ace203ba
sergey.dyachenko <sergey.dyachenko@aeroclub.ru>
2020-02-10 21:06:43 +0300

    upgrade xamarin version

--- QuestHelper/QuestHelper/ViewModel/RouteCarouselRootViewModel.cs
+++ QuestHelper/QuestHelper/ViewModel/RouteCarouselRootViewModel.cs
@@ -186,7 +186,7 @@ namespace QuestHelper.ViewModel
             return _userId;
         }
 
-        public int CarouselRowHeight => Convert.ToInt32(Consts.DeviceSize.FullScreenHeight * 0.6);
+        public int CarouselRowHeight => Convert.ToInt32(Consts.DeviceSize.FullScreenHeight * 0.7);
 
         public string RouteName
         {
