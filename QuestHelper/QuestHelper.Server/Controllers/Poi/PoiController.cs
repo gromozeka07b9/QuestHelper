455b4bddb670ebb9b7bb4264b524743ebe72b18b
sergey.dyachenko <sergey.dyachenko@aeroclub.ru>
2020-03-09 00:29:32 +0300

    Получение картинок для POI

--- QuestHelper/QuestHelper.Server/Controllers/Poi/PoiController.cs
+++ QuestHelper/QuestHelper.Server/Controllers/Poi/PoiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -20,7 +21,14 @@ namespace QuestHelper.Server.Controllers
     public class PoiController : Controller
     {
         private DbContextOptions<ServerDbContext> _dbOptions = ServerDbContext.GetOptionsContextDbServer();
+        private MediaManager _mediaManager;
+        private string _pathToMediaCatalog;
 
+        public PoiController()
+        {
+            _mediaManager = new MediaManager();
+            _pathToMediaCatalog = _mediaManager.PathToMediaCatalog;
+        }
         /// <summary>
         /// Get all available POIs for current user - published other people, private
         /// </summary>
@@ -127,6 +135,35 @@ namespace QuestHelper.Server.Controllers
             TimeSpan delay = DateTime.Now - startDate;
             Console.WriteLine($"UpdatePoi: status 200, {userId}, delay:{delay.TotalMilliseconds}");
         }
+
+        /// <summary>
+        /// Получение самого маленького превью, для отображения на карте
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{poiId}/image/{filename}")]
+        public IActionResult GetPoiImage(string poiId, string filename)
+        {
+            DateTime startDate = DateTime.Now;
+            string userId = IdentityManager.GetUserId(HttpContext);
+
+            using (var db = new ServerDbContext(_dbOptions))
+            {
+                var poi = db.Poi.Find(poiId);
+                if(!string.IsNullOrEmpty(poi?.ImgFilename) && poi.ImgFilename.Equals(filename))
+                {
+                    FileStream fileStream = System.IO.File.OpenRead(Path.Combine(_pathToMediaCatalog, filename));
+                    TimeSpan delay = DateTime.Now - startDate;
+                    Console.WriteLine($"GetPoiImage: status 200, {userId}, delay:{delay.TotalMilliseconds}");
+                    return new FileStreamResult(fileStream, "image/jpeg");
+                }
+                else
+                {
+                    Response.StatusCode = 404;
+                }
+            }
+            return new ObjectResult(null);
+        }
+
         private List<SharedModelsWS.Poi> selectPois(PoiFilter filter)
         {
             List<SharedModelsWS.Poi> pois = new List<SharedModelsWS.Poi>();
