e744256ec24fdfbe35c2ae48c7498c9992575100
Sergey Dyachenko <sdyachenko1977@gmail.com>
2020-08-16 00:33:00 +0300

    Генерация маршрутов по дням

--- QuestHelper/QuestHelper/Managers/ImagesCacheDbManager.cs
+++ QuestHelper/QuestHelper/Managers/ImagesCacheDbManager.cs
@@ -16,27 +16,29 @@ namespace QuestHelper.Managers
         private readonly IImageManager _imageManager;
         private DateTime _dateEnd;
         private DateTime _dateBegin;
+        private LocalFileCacheManager _cacheManager;
 
         public ImagesCacheDbManager(IImageManager imageManager, DateTime dateBegin, DateTime dateEnd)
         {
             _imageManager = imageManager;
             _dateBegin = dateBegin;
             _dateEnd = dateEnd;
+            _cacheManager = new LocalFileCacheManager();
         }
 
         internal void UpdateFilenames()
         {
             var startDate = DateTime.Now;
-            LocalFileCacheManager cacheManager = new LocalFileCacheManager();
+            //LocalFileCacheManager cacheManager = new LocalFileCacheManager();
             string pathToDCIMDirectory = DependencyService.Get<IPathService>().PublicDirectoryDcim;
 
             var listFiles = System.IO.Directory.EnumerateFiles(pathToDCIMDirectory, "*.jpg", SearchOption.AllDirectories);
             foreach(string filename in listFiles)
             {
                 var fileInfo = new FileInfo(filename);
-                if(!cacheManager.Exist(fileInfo.Name, fileInfo.CreationTime))
+                if(!_cacheManager.Exist(fileInfo.Name, fileInfo.CreationTime))
                 {
-                    cacheManager.Save(new ViewLocalFile() {Id = Guid.NewGuid().ToString(), SourceFileName = fileInfo.Name, SourcePath = fileInfo.DirectoryName, FileNameDate = fileInfo.CreationTime });
+                    _cacheManager.Save(new ViewLocalFile() {Id = Guid.NewGuid().ToString(), SourceFileName = fileInfo.Name, SourcePath = fileInfo.DirectoryName, FileNameDate = fileInfo.CreationTime });
                 }
             }
             var delay = DateTime.Now - startDate;
@@ -45,8 +47,8 @@ namespace QuestHelper.Managers
         internal void UpdateMetadata()
         {
             var startDate = DateTime.Now;
-            LocalFileCacheManager cacheManager = new LocalFileCacheManager();
-            var files = cacheManager.LocalFilesByDays(_dateBegin, _dateEnd);
+            //LocalFileCacheManager cacheManager = new LocalFileCacheManager();
+            var files = _cacheManager.LocalFilesByDays(_dateBegin, _dateEnd);
             foreach(var currentFile in files)
             {
                 if(!currentFile.Processed)
@@ -59,11 +61,20 @@ namespace QuestHelper.Managers
                         currentFile.ImagePreviewFileName = ImagePathManager.GetImagePath(currentFile.Id, LocalDB.Model.MediaObjectTypeEnum.Image, true);
                     }
                     currentFile.Processed = true;
-                    cacheManager.Save(currentFile);
+                    _cacheManager.Save(currentFile);
                 }
             }
 
             var delay = DateTime.Now - startDate;
+        }
+
+        internal int GetCountImagesForDaysAgo(int countDaysAgo)
+        {
+            var currentDate = DateTime.Now;
+            var dateEnd = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 23, 59, 59);
+            var dateStart = (new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 0, 0, 0)).AddDays(-countDaysAgo);
+            int count = _cacheManager.GetCountImagesByDay(dateStart, dateEnd).ToList().Sum(x=>x.Item2);
+            return count;
         }
     }
 }
