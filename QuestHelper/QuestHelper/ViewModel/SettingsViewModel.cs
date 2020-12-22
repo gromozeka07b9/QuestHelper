b3ef149bdad4d8e4ecbb52af5ed0ae7b76ec48f1
Sergey Dyachenko <sdyachenko1977@gmail.com>
2020-12-22 22:42:21 +0300

    отображение списка файлов в настройках

--- QuestHelper/QuestHelper/ViewModel/SettingsViewModel.cs
+++ QuestHelper/QuestHelper/ViewModel/SettingsViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Windows.Input;
 using QuestHelper.Managers;
+using Syncfusion.DataSource.Extensions;
 using Xamarin.Forms;
 
 namespace QuestHelper.ViewModel
@@ -11,18 +16,65 @@ namespace QuestHelper.ViewModel
     {
         private bool _isUsageMainMemory;
         private bool _isUsageExtMemory;
+        private ObservableCollection<string> _sourcePath = new ObservableCollection<string>();
+        private string _pathToImagesDir = string.Empty;
 
         public INavigation Navigation { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand BackNavigationCommand { get; private set; }
         public ICommand MainMemoryCheckBoxCommand { get; private set; }
         public ICommand ExtMemoryCheckBoxCommand { get; private set; }
-
+        public ICommand NavigateDirUpCommand { get; private set; }
         public SettingsViewModel()
         {
             BackNavigationCommand = new Command(backNavigationCommand);
             MainMemoryCheckBoxCommand = new Command(mainMemoryCheckBoxCommand);
             ExtMemoryCheckBoxCommand = new Command(extMemoryCheckBoxCommand);
+            NavigateDirUpCommand = new Command(navigateDirUpCommand);
+        }
+
+        private void navigateDirUpCommand(object obj)
+        {
+            var currentDirectory = Directory.GetParent(_pathToImagesDir);
+            if (currentDirectory.Exists)
+            {
+                ObservableCollection<string> files = new ObservableCollection<string>();
+                ObservableCollection<string> dirs = new ObservableCollection<string>();
+                try
+                {
+                    files = currentDirectory.GetFiles("*.jpg;*.jpeg;*.png").OrderBy(f => f.CreationTime)
+                        .Select(f => f.Name).ToArray().Take(10).ToObservableCollection();
+                    dirs = currentDirectory.GetDirectories().OrderBy(d => d.Name).Select(d => d.Name)
+                        .ToObservableCollection();
+                    _pathToImagesDir = currentDirectory.FullName;
+                    SourcePaths = new ObservableCollection<string>(dirs);
+                    CountOfPhotoInternalDCIM = files.Count();
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CountOfPhotoInternalDCIM"));
+                    int index = 0;
+                    while (index < 10)
+                    {
+                        if (files.ElementAtOrDefault(index) != null)
+                        {
+                            SourcePaths.Add(files.ElementAt(index));
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        index++;
+                    }
+                }
+                catch (IOException ioException)
+                {
+                    
+                }
+                catch (Exception e)
+                {
+                    
+                }
+                
+            }
         }
 
         private void extMemoryCheckBoxCommand(object obj)
@@ -63,9 +115,11 @@ namespace QuestHelper.ViewModel
 
                 PathToInternalDCIM = pathToDCIMDirectory;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PathToInternalDCIM"));
+                _pathToImagesDir = pathToDCIMDirectory;
                 var files = imagesCache.GetListFiles(pathToDCIMDirectory);
                 CountOfPhotoInternalDCIM = files.Count();
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CountOfPhotoInternalDCIM"));
+                SourcePaths = new ObservableCollection<string>(files.ToList());
             }
         }
 
@@ -108,6 +162,22 @@ namespace QuestHelper.ViewModel
                 {
                     _isUsageExtMemory = value;
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsUsageExtMemory"));
+                }
+            }
+        }
+
+        public ObservableCollection<string> SourcePaths
+        {
+            get
+            {
+                return _sourcePath;
+            }
+            set
+            {
+                if (value != _sourcePath)
+                {
+                    _sourcePath = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SourcePaths"));
                 }
             }
         }
