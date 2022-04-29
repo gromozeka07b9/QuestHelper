e264bf22e724edd5a7bd919c78f881f14b279cfb
Sergey Dyachenko <sdyachenko1977@gmail.com>
2022-04-29 23:42:21 +0300

    added old onboarding

--- QuestHelper/Lighter/Components/OnboardingCarousel/OnboardingCarouselPage.xaml.cs
+++ QuestHelper/Lighter/Components/OnboardingCarousel/OnboardingCarouselPage.xaml.cs
@@ -17,26 +17,54 @@ namespace Lighter.Components.OnboardingCarousel
         {
             this.onboardingCarouselModel = onboardingCarouselModel;
             InitializeComponent();
+            BuildContent();
+            
         }
 
         private void OnboardingCarouselPage_OnAppearing(object sender, EventArgs e)
         {
             foreach (var screen in onboardingCarouselModel.OnboardingScreens)
             {
-                var contentPage = new ContentPage();
-                var grid = new Grid();
+                /*var grid = new Grid();
                 grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1, GridUnitType.Star)});
                 grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(8, GridUnitType.Star)});
                 grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1, GridUnitType.Star)});
                 grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(5, GridUnitType.Star)});
                 grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(3, GridUnitType.Star)});
-                grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(2, GridUnitType.Star)});
-                var label = new Label();
-                label.Text = screen.Headline.Content;
-                grid.Children.Add(label, 1, 1);
-                contentPage.Content = grid;
-                this.Children.Add(contentPage);
+                grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(2, GridUnitType.Star)});*/
+                //var label = new Label();
+                //label.Text = screen.Headline.Content;
+                /*var contentPage = new ContentPage();
+                var onboardingView = new OnboardingView();
+                onboardingView.Text = screen.Headline.Content;
+                onboardingView.ImgSource = screen.Image.Filename;
+                //grid.Children.Add(onboardingView, 1, 1);
+                contentPage.Content = onboardingView;
+                this.Children.Add(contentPage);*/
             }
+        }
+
+        void BuildContent()
+        {
+            foreach (var screen in onboardingCarouselModel.OnboardingScreens)
+            {
+                var contentPage = new ContentPage();
+                /*var grid = new Grid();
+                grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1, GridUnitType.Star)});
+                grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(8, GridUnitType.Star)});
+                grid.ColumnDefinitions.Add(new ColumnDefinition(){Width = new GridLength(1, GridUnitType.Star)});
+                grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(5, GridUnitType.Star)});
+                grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(3, GridUnitType.Star)});
+                grid.RowDefinitions.Add(new RowDefinition(){Height = new GridLength(2, GridUnitType.Star)});*/
+                //var label = new Label();
+                //label.Text = screen.Headline.Content;
+                var onboardingView = new OnboardingView();
+                onboardingView.Text = screen.Headline.Content;
+                onboardingView.ImgSource = screen.Image.Filename;
+                //grid.Children.Add(onboardingView, 1, 1);
+                contentPage.Content = onboardingView;
+                this.Children.Add(contentPage);
+            }            
         }
     }
 }
